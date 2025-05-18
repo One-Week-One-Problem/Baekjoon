6f1d8e20015eb1160bc8cf484e57f294abd86ae1
chopinbag <sw6129@pusan.ac.kr>
2025-05-18 15:31:37 +0900

    31791 : Logic complete

--- Chopin_Bag/31791/31791.cpp
+++ Chopin_Bag/31791/31791.cpp
@@ -9,8 +9,7 @@ using namespace std;
 
 // 감염상태
 enum class State{
-    NORMAL = 0, // NORMAL 은 아마 쓰이지 않음
-    PROPAGATING,
+    PROPAGATING = 0,
     INFECTED
 };
 
@@ -18,8 +17,8 @@ enum class State{
 class Virus {
     public:
         State state_;
-        int row_, col_; // public으로 변경 또는 getter 함수 제공 필요
-        int infection_lv_=0; // public으로 변경 또는 getter/setter 제공 필요
+        int row_, col_;
+        int infection_lv_= 1;
 
         Virus()= default;
         Virus(int row, int col, State state=State::INFECTED)
@@ -44,9 +43,9 @@ class Virus {
                    state_ == other.state_; // 상태도 동일성 판단에 포함
         }
 
-        vector<Virus> propagate(const vector<vector<bool>>& is_building, const int time_delay, 
+        vector<Virus> propagate(const vector<vector<bool>> is_building, const int time_delay, 
             const int size_row, const int size_col,
-            const vector<Virus>& current_viruses, const vector<Virus>& temp_new_viruses){ // 파라미터 변경
+            const vector<Virus>& current_viruses, const vector<Virus>& temp_new_viruses){ 
 
             vector<Virus> ret_vec;
 
@@ -62,17 +61,19 @@ class Virus {
                 return ret_vec; // 이번 턴 전파 없음
             }
 
-            if(state_ == State::PROPAGATING){ // 건물 아닌 곳의 PROPAGATING
-               state_ = State::INFECTED; // 상태만 변경
-               // cout << get_status() << "[STATUS] P -> I" << endl; // 디버깅용 출력
-               return ret_vec; // 이번 턴 전파 없음
-            }
+            // 건물이 아닌 곳에서 Propagate 함수가 실행되었다는 것은, 이미 state_ == State::INFECTED 라는 뜻이 아닐까?
+
+            // if(state_ == State::PROPAGATING){ // 건물 아닌 곳의 PROPAGATING
+            //    state_ = State::INFECTED; // 상태만 변경
+            //    cout << get_status() << "[STATUS] P -> I" << endl; // 디버깅용 출력
+            //    return ret_vec; // 이번 턴 전파 없음
+            // }
 
             // INFECTED 상태일 때만 전파 시도
             if (state_ == State::INFECTED) {
                 vector<pair<int,int>> offsets{{1,0}, {0,1}, {-1,0}, {0,-1}};
                 
-                // cout << "_____________________________________________________________________" << endl;
+                // cout << "_____________________________________________________________________" << endl; // 디버깅용 출력
                 // cout << "[TEST] " << get_status() << " is INFECTED and trying to spread\n" << endl; // 디버깅용 출력
                 
                 for (auto p : offsets){
@@ -82,7 +83,8 @@ class Virus {
                     // cout << "[OFFSET] (" << p.first << "," << p.second << ")" << " | "; // 디버깅용 출력
 
                     if(status_check(next_row, next_col, size_row, size_col, current_viruses, temp_new_viruses)){
-                        ret_vec.emplace_back(Virus(next_row, next_col, State::PROPAGATING));
+                        if(is_building[next_row][next_col]) ret_vec.emplace_back(Virus(next_row, next_col, State::PROPAGATING));
+                        else ret_vec.emplace_back(Virus(next_row, next_col, State::INFECTED)); 
                     }
                 }
             }
@@ -124,10 +126,6 @@ class Virus {
         string get_status(){
             return "(" + to_string(row_+1) + "," + to_string(col_+1) + ")";
         }
-
-    // private: // 멤버 변수 접근을 위해 public으로 변경 또는 getter/setter 필요
-    // int row_, col_;
-    // int infection_lv_=0;
 };
 
 
@@ -138,23 +136,22 @@ int main() {
     cout.tie(NULL);
     
     // 디버깅용 입력
-    int grid_row = 5, grid_col = 5, time_delay = 1, time_total = 3;
-    // int num_init = 2, num_building = 3; // 이 변수들은 현재 사용되지 않음
-    vector<vector<char>> grid_input = { // grid_input으로 변경하여 is_building과 구분
-    {'.','.','.','.','.'},
-    {'.','.','*','#','.'},
-    {'.','*','#','.','.'},
-    {'.','.','#','.','.'}, 
-    {'.','.','.','.','.'}
-    };
-
+    int grid_row, grid_col, time_delay, time_total, num_init_virus, num_building;
+    cin >> grid_row >> grid_col;
+    cin >> time_total >> time_delay >> num_init_virus >> num_building;
+    
     vector<Virus> viruses; // 바이러스 객체 벡터
-    vector<vector<bool>> is_building(grid_row, vector<bool>(grid_col, false)); // 빌딩이 있는가?
+    vector<vector<bool>> is_building(grid_row, vector<bool>(grid_col, false)); // 빌딩이 있는가?    
+
+    vector<vector<char>> grid_input(grid_row, vector<char>(grid_col)); // grid_input으로 변경하여 is_building과 구분
+
+    
 
     for (int i = 0; i < grid_row; ++i) {
         for (int j = 0; j < grid_col; ++j) {
+            cin >> grid_input[i][j]; // 각 문자를 grid_input에 직접 저장
             if (grid_input[i][j] == '*') {
-                viruses.emplace_back(Virus(i, j, State::INFECTED)); // 초기 감염 바이러스
+                viruses.emplace_back(Virus(i, j, State::INFECTED)); // 초기 바이러스는 INFECTED
             } else if (grid_input[i][j] == '#') {
                 is_building[i][j] = true;
             }
@@ -162,7 +159,7 @@ int main() {
     }
 
     // 시뮬레이션 시작 : 전체 반복
-    for(int t = 0; t < time_total; ++t){
+    for(int t = 0; t < time_total; ++t){ // t < time_total 로 바꿀 것
         vector<Virus> temp_newly_propagated_viruses; // 이번 턴에 새로 전파된 바이러스들
         
         size_t current_virus_count = viruses.size(); // 루프 시작 시점의 바이러스 수
@@ -180,10 +177,38 @@ int main() {
         viruses.erase(std::unique(viruses.begin(), viruses.end()), viruses.end());
     }
 
-    // 바이러스 현황 출력 (문제의 요구사항에 따라 INFECTED 상태만 출력할 수도 있음)
-    for(auto& v : viruses){ // const auto& 사용
-        cout << v.get_status() << endl;
+    // INFECTED 상태의 바이러스 좌표 출력 - 디버깅용 출력
+    // cout << "\n\nFinal OUTPUT\n" << endl; // 디버깅용 출력
+    // for(auto& v : viruses){ // const auto& 사용
+    //     if(v.state_ == State::INFECTED) cout << v.get_status() << endl;
+    //     if(v.state_ == State::PROPAGATING) cout << "[Propagating]" << v.get_status() << ", lv: " << v.infection_lv_ << endl;
+    // }
+
+    vector<vector<bool>> is_infected_grid(grid_row, vector<bool>(grid_col, false));
+    int is_exist = grid_col * grid_row;
+    for (const auto& virus : viruses) {
+        if (virus.state_ == State::INFECTED) {
+            if (virus.row_ >= 0 && virus.row_ < grid_row && virus.col_ >= 0 && virus.col_ < grid_col) { // 혹시 모를 범위 체크
+                is_infected_grid[virus.row_][virus.col_] = true;
+                is_exist--;
+            }
+        }
     }
 
+    if(is_exist==0){
+        cout << -1;
+        return 0;
+    }
+
+    bool found_safe_zone = false;
+    for (int r = 0; r < grid_row; ++r) {
+        for (int c = 0; c < grid_col; ++c) {
+            if (!is_infected_grid[r][c]) {
+                // 문제에서 요구하는 출력 형식 (예: "행번호 열번호")
+                cout << r + 1 << " " << c + 1 << "\n";
+                found_safe_zone = true;
+            }
+        }
+    }
     return 0;
 }
